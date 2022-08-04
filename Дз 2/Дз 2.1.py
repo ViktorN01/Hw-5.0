218cef184d1d07707b213dd96be6e3fb0a0c7247
ViktorN01 <mr.valdes334@gmail.com>
2022-08-04 19:08:32 +0300

    Дз 2.1

--- Дз 2/Дз 2.1.py
+++ Дз 2/Дз 2.1.py
@@ -1,3 +1,3 @@
-list = (1, 'dva', 3.3, 4+4j, True, [1, 2], (1, 2), {1, 2})
+list = (1, 'dva', 3.3, 4+4j, True, [1, 2], (1, 2), {1: 'one', 2: 'two'}, {9, 10}, frozenset(), range(12), b'thirdteen', bytearray(b'fourteen'), TypeError)
 type = type(list[3])
 print(type)
