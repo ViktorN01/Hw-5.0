8a96fa3488741748affb1d7a9977fd162bf81949
ViktorN01 <mr.valdes334@gmail.com>
2022-08-30 19:20:29 +0300

    Hw-5.0

--- Дз 5/5.1.py
+++ Дз 5/5.1.py
@@ -1,7 +1,1 @@
-with open('counts', 'w', encoding='utf-8') as name:
-    while True:
-        line = input('Введите ваши данные ')
-        if not line:
-            break
 
-        print(line, file=name)
